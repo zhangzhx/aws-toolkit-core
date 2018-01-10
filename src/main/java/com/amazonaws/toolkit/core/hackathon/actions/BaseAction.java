0595e37d7a723c783ad183cbf17770e95219c29f
Zhaoxi Zhang <zhaoxiz@amazon.com>
2018-01-10 11:45:55 -0800

    More comments and some clean up.

--- src/main/java/com/amazonaws/toolkit/core/hackathon/actions/BaseAction.java
+++ src/main/java/com/amazonaws/toolkit/core/hackathon/actions/BaseAction.java
@@ -4,40 +4,32 @@ import java.io.IOException;
 
 import com.amazonaws.toolkit.core.hackathon.ActionContext;
 import com.amazonaws.toolkit.core.hackathon.ActionInfo;
-import com.amazonaws.toolkit.core.hackathon.IActionExecutionContextProvider;
 import com.amazonaws.toolkit.core.hackathon.analytics.ToolkitEvent;
-import com.amazonaws.toolkit.core.hackathon.analytics.ui.EventPublisherProvider;
 import com.amazonaws.toolkit.core.hackathon.models.ActionException;
 import com.amazonaws.toolkit.core.hackathon.models.ActionInput;
 import com.amazonaws.toolkit.core.hackathon.models.ActionOutput;
 
 public abstract class BaseAction<I extends ActionInput, O extends ActionOutput, E extends ActionException> {
     private final ActionInfo actionInfo;
-    private final ToolkitEvent metrics;
-    private final IActionExecutionContextProvider<I> contextProvider;
 
-    protected BaseAction(ActionInfo actionInfo, IActionExecutionContextProvider<I> contextProvider) {
+    protected BaseAction(ActionInfo actionInfo) {
         this.actionInfo = actionInfo;
-        this.contextProvider = contextProvider;
-        this.metrics = EventPublisherProvider.INSTANCE.getEventPublisher().createEvent(actionInfo.getName());
     }
 
-    public O execute() throws E {
-        I input = contextProvider.createActionInput();
-        ActionContext context = createActionContext();
+    public O execute(I input, ActionContext context) throws E {
+        ToolkitEvent metrics = context.getEvent();
         try {
             long startTimeMilli = System.currentTimeMillis();
             O output = doExecute(input, context);
             long time = System.currentTimeMillis() - startTimeMilli;
             metrics.addMetric("Execution time milli", time);
-            if (output.isSucceeded()) {
-                metrics.addAttribute("Result", output.getResult().name());
-            }
+            metrics.addAttribute("Result", output.getResult().name());
             return output;
         } catch (ActionException e) {
             metrics.addAttribute("Result", ActionOutput.ActionResult.FAILED.name());
             throw e;
         } finally {
+            // TODO to be removed in the prod code
             ToolkitEvent.print(context.getLogger(), metrics);
             metrics.record();
             try {
@@ -50,12 +42,6 @@ public abstract class BaseAction<I extends ActionInput, O extends ActionOutput, 
 
     public ActionInfo getActionInfo() {
         return this.actionInfo;
-    }
-
-    private ActionContext createActionContext() {
-        return new ActionContext(metrics,
-                contextProvider.createLogger(),
-                contextProvider.createProgresser());
     }
 
     protected abstract O doExecute(I input, ActionContext context) throws E;
--- src/main/java/com/amazonaws/toolkit/core/hackathon/actions/FooAction.java
+++ src/main/java/com/amazonaws/toolkit/core/hackathon/actions/FooAction.java
@@ -1,8 +1,9 @@
 package com.amazonaws.toolkit.core.hackathon.actions;
 
+import java.util.Random;
+
 import com.amazonaws.toolkit.core.hackathon.ActionContext;
 import com.amazonaws.toolkit.core.hackathon.ActionInfo;
-import com.amazonaws.toolkit.core.hackathon.IActionExecutionContextProvider;
 import com.amazonaws.toolkit.core.hackathon.ToolkitLogger;
 import com.amazonaws.toolkit.core.hackathon.ToolkitProgresser;
 import com.amazonaws.toolkit.core.hackathon.analytics.ToolkitEvent;
@@ -11,14 +12,17 @@ import com.amazonaws.toolkit.core.hackathon.models.ActionOutput;
 import com.amazonaws.toolkit.core.hackathon.models.ActionOutput.ActionResult;
 import com.amazonaws.toolkit.core.hackathon.models.FooInput;
 
+/**
+ * An fake action running for 10 seconds with 1/12 possibility to fail.
+ */
 public class FooAction extends BaseAction<FooInput, ActionOutput, ActionException>{
 
-    public FooAction(IActionExecutionContextProvider<FooInput> provider) {
-        super(ActionInfo.FOO, provider);
+    public FooAction() {
+        super(ActionInfo.FOO);
     }
 
     @Override
-    public ActionOutput doExecute(FooInput input, ActionContext context) throws ActionException {
+    protected ActionOutput doExecute(FooInput input, ActionContext context) throws ActionException {
         ToolkitEvent metrics = context.getEvent();
         ToolkitProgresser progresser = context.getProgresser();
         ToolkitLogger logger = context.getLogger();
@@ -26,7 +30,6 @@ public class FooAction extends BaseAction<FooInput, ActionOutput, ActionExceptio
         progresser.beginTask("Executing Foo Action");
         for (int i = 0; i < 10; i++) {
             if (progresser.isCanceled()) {
-                // TODO clean up the task safely
                 logger.error("The action is canceled at %d seconds.\n", i);
                 metrics.addMetric("Execute Times", i);
                 return new ActionOutput(ActionResult.CANCELED);
@@ -34,10 +37,10 @@ public class FooAction extends BaseAction<FooInput, ActionOutput, ActionExceptio
             try {
                 Thread.sleep(1000);
                 logger.info("I have slept for %d seconds in project %s.\n", i+1, input.getProjectName());
-//                if (new Random().nextInt(12) == 1) {
-//                    throw new ActionException("Random assertion failed!", null);
-//                }
-                progresser.workedFraction(0.1);
+                if (new Random().nextInt(12) == 1) {
+                    throw new ActionException("Random assertion failed!", null);
+                }
+                progresser.workedFraction(0.1 * (i + 1));
             } catch (InterruptedException e) {
                 // Allowed exception, we handle here.
             }
