f94d2725ef1f2d09ac6a0ace1e8bed77dceea6a5
Zhaoxi Zhang <zhaoxiz@amazon.com>
2019-05-14 13:18:51 -0700

    Master new commit 2

--- src/main/java/com/amazonaws/toolkit/core/hackathon/example/FooActionRun.java
+++ src/main/java/com/amazonaws/toolkit/core/hackathon/example/FooActionRun.java
@@ -3,6 +3,7 @@ package com.amazonaws.toolkit.core.hackathon.example;
 import com.amazonaws.toolkit.core.hackathon.actions.FooAction;
 import com.amazonaws.toolkit.core.hackathon.models.FooInput;
 
+// Naother
 public class FooActionRun extends AbstractActionRun<FooInput> {
 
  protected FooActionRun() {
